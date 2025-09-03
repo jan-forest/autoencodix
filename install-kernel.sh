5e7143aa3c4a093ebc68cb63aed21b50c81f232d
Neringa Jurenaite <neringa.jurenaite@tu-dresden.de>
2025-09-03 17:04:35 +0200

    updated install-kernel to include sim link to gallia

--- install-kernel.sh
+++ install-kernel.sh
@@ -15,8 +15,8 @@ echo " Setting Kernel"
 echo "=============================================================="
 # Sourcing the virtual environment
 VENV_PATH="/data/horse/ws/neju219d-AUTOENCODIX/autoencodix/venv-AUTOENCODIX-capella-no-ssp"
-#VENV_NAME="venv-AUTOENCODIX-capella-no-ssp"
-echo "Activating virtual environment: $VENV_PATH"
+VENV_NAME="venv-AUTOENCODIX-capella-no-ssp"
+echo "Activating virtual environment: $VENV_NAME"
 source $VENV_PATH/bin/activate
 
 # Installing kernel
@@ -67,7 +67,7 @@ EOF
 chmod u+rwx $KERNEL_INIT_SCRIPT
 
 # Replace the contents of kernel.json file
-echo "Replacing contents of $PYTHON_KERNEL_DIR/kernel.json"
+# echo "Replacing contents of $PYTHON_KERNEL_DIR/kernel.json"
 cat << EOF > "$PYTHON_KERNEL_DIR/kernel.json"
 {
   "argv": [
@@ -86,18 +86,23 @@ echo "Kernel installation complete."
 
 
 echo "=============================================================="
-echo " Resetting modules & env, creating & registering  workspace"
+echo " Creating & registering AE-ws workspace"
 echo "=============================================================="
 deactivate
 module reset
 sleep 1s
+
+ws_allocate -F horse AE-ws 10
+ws_register -F horse $HOME
+ln -s $VENV_PATH $HOME/horse/neju219d-AE-ws/autoencodix/venv-gallia
+#ln -s venv-AUTOENCODIX-capella-no-ssp venv-gallia
 
 
 echo "=============================================================="
 echo " Your next steps"
 echo "=============================================================="
 echo "1. Re-load the browser"
-echo "2. Relocate to Tutorials in the directory view on the left side"
+echo "2. Relocate to directory /horse/$USER-AE-ws/autoencodix/Tutorials"
 echo "3. Open Setup_InputFormat.ipynb notebook"
 echo "4. Select the \"$PYTHON_KERNEL_NAME\": Top Menu -> Kernel -> Change Kernel"
 
